4ab68e5a47551b4354f94dff13a9897b8fcbc32f
Kateryna Vasylieva <catarina.leon.203@gmail.com>
2023-03-16 15:11:17 +0200

    +slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 //вывод всех функций в flsFunctions
 import * as flsFunctions from "./modules/functions.js";
+import * as flsSlider from "./modules/slider.js";
 
 //проверка на поддержку Webp изображений и метка классов "webp" или "no-webp"
 flsFunctions.isWebp();
+flsSlider.slickSlider()
---
+++ src/js/modules/slider.js
@@ -0,0 +1,70 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+export function slickSlider(){
+$('.container__slider').slick({
+	arrows:false,
+    dots: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	speed: 1000,
+	// тип анимации
+	easing: 'ease',
+	// бесконечная прокрутка слайдов
+	infinite: false,
+	// стартовый слайд, по умолчанию 0
+	initialSlide:0,
+	// Автоматическая адаптивная высота слайдера => adaptiveHeight
+	adaptiveHeight: true,
+	responsive: [
+		{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					dots: false,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			}
+	],
+	// mobileFirst:false,
+	// автоматичеая прокрутка слайдов => autoplay по умол false
+	// autoplay: false,
+	// скорость прокрутки => autoplaySpeed
+	// autoplaySpeed:3000,
+	// пауза при наведении => pauseOnHover
+	// pauseOnHover: true,
+	// пауза в фокусе => pauseOnFocus
+	// pauseOnFocus:true,
+	// пауза при наведении на точки => pauseOnDotsHover
+	// pauseOnDotsHover: true,
+	// не свайпать мышкой => draggable
+	// draggable:false,
+	// не свайпать на моб устройствах => swipe
+	// swipe:false,
+	// touchThreshold:5,
+	// touchMove:true,
+	// быстро перелистивать слайды
+	// waitForAnimate: false,
+	// первый слайд по центру
+	// centerMode:false,
+	// variableWidth:false,
+	// rows:1,
+	// slidesPerRow:2,
+	// vertical: true,
+	// verticalSwiping: true,
+})
+}
